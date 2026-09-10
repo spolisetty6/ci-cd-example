bb20ba60e2f8a09784f2de50d58cdc19cfd39c9b
Sreeni Polisetty <65161331+spolisetty6@users.noreply.github.com>
2026-09-10 17:01:47 +0100

    Move Jest config to node-app directory

---
+++ node-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts'
+  ]
+};
